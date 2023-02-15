a2105644dbd2a0d6fb657277f38ad42ab318698a
Xiaolu <kongbingji@163.com>
2023-02-15 09:51:24 +0800

    control

---
+++ src/main/java/com/comit/course/_01_/operator/Assignment.java
@@ -0,0 +1,14 @@
+package com.comit.course._01_.operator;
+
+class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =  4;
+		
+		a = a + 5;
+		a += 5;
+		System.out.println(a);
+	}
+
+}
---
+++ src/main/java/com/comit/course/_02_/control/IfStatement.java
@@ -0,0 +1,20 @@
+package com.comit.course._02_.control;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+		int b = 20;
+		
+		if ( a < b ) {
+			System.out.println("a is greater than b");
+		} else if (a == b) {
+			System.out.println("a equals to b");
+		}else {
+			System.out.println("a is less than b");
+
+		}
+	}
+
+}
